23050e6cd044f16a3e2930fa102611f4a9ad506b
Mikael H <mikael.holsve@gmail.com>
2022-12-05 06:37:58 +0100

    Implementerede "gemMeldemsData()". Implementation af test

--- src/Medlem.java
+++ src/Medlem.java
@@ -1,20 +1,24 @@
 package src;
 import java.util.ArrayList;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Medlem {
 
-	public static ArrayList<Medlem> MedlemListe;
+	public static ArrayList<Medlem> MedlemListe = new ArrayList<Medlem>();
 	private int id;
 	private String navn;
 	private LocalDate foedselsdato;
+	private boolean aktiv;
 	private String aktivitetsform;
 	private int kontingentId;
 
-	public Medlem(int id, String navn, LocalDate foedselsdato, String aktivitetsform) {
+	public Medlem(int id, String navn, LocalDate foedselsdato, boolean aktiv, String aktivitetsform) {
 		this.id = id;
 		this.navn = navn;
 		this.foedselsdato = foedselsdato;
+		this.aktiv = aktiv;
 		this.aktivitetsform = aktivitetsform;
 	}
 
@@ -28,9 +32,19 @@ public class Medlem {
 		throw new UnsupportedOperationException();
 	}
 
-	public static void gemMedlemsdata() {
-		// TODO - implement Medlem.gemMedlemsdata
-		throw new UnsupportedOperationException();
+	public static void gemMedlemsdata() throws IOException {
+		String[] medlemArray = new String[MedlemListe.size()];
+		int i = 0;
+		for(Medlem m : MedlemListe) {
+			medlemArray[i] = m.id + ";"
+				+ m.navn + ";"
+				+ m.foedselsdato.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + ";"
+				+ m.aktiv + ";"
+				+ m.aktivitetsform;
+			i++;
+		}
+
+		FileHandler.WriteToFile("./medlemmer.txt", medlemArray);
 	}
 
 }
--- test/MedlemTest.java
+++ test/MedlemTest.java
@@ -1,7 +1,9 @@
 package test;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
-import java.util.Date;
+import java.util.Scanner;
 
 import org.junit.Test;
 import src.Medlem;
@@ -9,13 +11,23 @@ import src.Medlem;
 public class MedlemTest {
     
     @Test
-    public void gemMedlemsdataTest() {
-        Medlem m1 = new Medlem(0, "Test Testesen", LocalDate.of(1997, 11, 17), null);
-        Medlem m2 = new Medlem(1, "Lars Larsen", LocalDate.of(1978, 02, 05), null);
+    public void gemMedlemsdataTest() throws IOException {
+        Medlem m1 = new Medlem(0, "Test Testesen", LocalDate.of(1997, 11, 17), true, null);
+        Medlem m2 = new Medlem(1, "Lars Larsen", LocalDate.of(1978, 02, 05), true, null);
 
         Medlem.MedlemListe.add(m1);
         Medlem.MedlemListe.add(m2);
 
         Medlem.gemMedlemsdata();
+
+        // Read file. Confirm that content is OK.
+        String content = "";
+        Scanner scan = new Scanner(new File("./medlemmer.txt"));
+        scan.useDelimiter("\n");
+        while(scan.hasNext()) {
+            content += scan.next();
+        }
+
+        assert(content.equals("0;Test Testesen;17/11/1997;true;null1;Lars Larsen;05/02/1978;true;null"));
     }
 }
