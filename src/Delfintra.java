ff014f82a2fd61c0abc9edf80b8766eca03cded6
EliasF2 <118987010+EliasF2@users.noreply.github.com>
2022-12-05 22:08:28 +0100

    meget flot do while loop

--- src/Delfintra.java
+++ src/Delfintra.java
@@ -1,6 +1,7 @@
 package src;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Delfintra {
 
@@ -9,61 +10,67 @@ public class Delfintra {
 	}
 
 	public static void visValgmuligheder() {
-		String newLine = System.getProperty("line.separator");
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Hvad vil du?" + newLine);
-        System.out.println("1 - Registrer Aktivitetsform" + newLine + "2 - Registrer Stamoplysninger" + newLine + "3 - Registrer Indbetalling");
-        System.out.println("4 - Liste over medlemmer i restance" + newLine + "5 - Registrer konkurrence svoemmere's (Stævne/placering/tid)");
-        System.out.println("6 - Registrer træningsresultater" + newLine + "7 - Top 5 svømmere for hver disciplin" + newLine);
+        System.out.println("-----Hvad vil du?-----");
+        System.out.println("1 - Registrer medlem \n2 - Registrer Indbetalling");
+        System.out.println("3 - Liste over medlemmer i restance \n4 - Registrer konkurrence svoemmere's (Stævne/placering/tid)");
+        System.out.println("5 - Registrer træningsresultater \n6 - Se top 5 svømmere for hver disciplin");
 
-        int tastTal = input.nextInt();
-        if(tastTal > 7 || tastTal < 1){System.out.println("Venligst indtast et gyldigt tal");}
-          
-        switch(tastTal){
-            //Registrer Aktivitetsform
-            case 1:
-            System.out.println("Du har valgt at Registrer Aktivitesform");
-            //indtast kode her  
-            break;
+        int tastTal;
+        
 
-            //Registrer Stamoplysninger
-            case 2:
-            System.out.println("Du har valgt at Registrer Stamoplysninger");
+        ArrayList<Integer> valid_answers = new ArrayList<>();
+        for (int i=1; i<=6; i++) {
+            valid_answers.add(i);
+        }
 
-            break;
+        do {
+            tastTal = input.nextInt();
+            
+            if(tastTal > 7 || tastTal < 1){
+                System.out.println("Venligst indtast et gyldigt tal");
+            }
 
-            //Registrer indbetalling
-            case 3:
-            System.out.println("Du har valgt at Registrer Indbetallinger");
+            switch(tastTal){
+                //Registrer medlem
+                case 1:
+                System.out.println("Du har valgt at registrer et medlem");
 
-            break;
+                break;
 
-            //Liste af medlemmer i restance
-            case 4:
-            System.out.println("Du har valgt at se en liste over medlemmer i restance");
+                //Registrer indbetalling
+                case 2:
+                System.out.println("Du har valgt at Registrer Indbetallinger");
 
-            break;
+                break;
 
-            //Konkurrence svoemmere's resultater
-            case 5:
-            System.out.println("Du har valgt at Registrer konkurrence svoemmere's resultater (Stævne/placering/tid)");
+                //Liste af medlemmer i restance
+                case 3:
+                System.out.println("Du har valgt at se en liste over medlemmer i restance");
 
-            break;
+                break;
 
-            //Registrer træningsresultater
-            case 6:
-            System.out.println("Du har valgt at Registrer træningsresultater");
+                //Konkurrence svoemmere's resultater
+                case 4:
+                System.out.println("Du har valgt at Registrer konkurrence svoemmere's resultater (Stævne/placering/tid)");
 
-            break;
+                break;
 
-            //top 5 liste 
-            case 7:
-            System.out.println("Du har valgt at se en top 5 liste");
+                //Registrer træningsresultater
+                case 5:
+                System.out.println("Du har valgt at Registrer træningsresultater");
 
-            break;
+                break;
 
-        }//Slut paa switch
+                //Se top 5 liste 
+                case 6:
+                System.out.println("Du har valgt at se en top 5 liste");
+
+                break;
+            } //Slut paa switch
+
+        } while (!valid_answers.contains(tastTal));
 	}
 
 }
