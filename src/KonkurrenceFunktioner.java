d75894bbeb23212a9c36e2454fdf2f24a837792d
Mikael H <mikael.holsve@gmail.com>
2022-12-09 12:23:17 +0100

    registrerTræning() - fejlhåndtering og andet
    Føjede fejlhåndtering. Ændrede måden medlemmer bliver valgt på til at man indtaster medlemmets ID direkte

--- src/KonkurrenceFunktioner.java
+++ src/KonkurrenceFunktioner.java
@@ -1,8 +1,10 @@
 package src;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -21,20 +23,57 @@ public class KonkurrenceFunktioner {
 	}
 
 
-	public static void registrerTraening() {
-		System.out.println("Hvilket medlem vil du registrere en tid for?");
-		int id = Medlem.findMedlem();
+	public static void registrerTraening() throws IOException {
+		System.out.print("\nMedlemsliste:\n\n");
+		Medlem.printMedlemListe();
+		
+		int id = -1;
+		boolean validInput = true;
+		do {
+			try {
+				validInput = true;
+				System.out.println("Hvilket medlem vil du registrere en tid for? Indtast vedkommendes ID:");
+				id = scan.nextInt();
+			} catch(InputMismatchException e) {
+				System.out.println("Inputtet skal være et heltal!");
+				scan.next();
+				validInput = false;
+			}
+		} while(!validInput);
+
 		System.out.println("Hvilken disciplin?");
-		String disciplin = scan.nextLine();
-		System.out.println("Hvor mange hele sekunder?");
-		int tid = scan.nextInt();
-		System.out.println("Indtast dato for resultatet (dd-mm-yyyy)");
-		String dato = scan.nextLine();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-		LocalDate resDato = LocalDate.parse(dato, formatter);
+		String disciplin = scan.next();
+		int tid = -1;
+		do {
+			System.out.println("Hvor mange hele sekunder?");
+			try {
+				validInput = true;
+				tid = scan.nextInt();
+			} catch(InputMismatchException e) {
+				System.out.println("Inputtet skal være et heltal!");
+				scan.next();
+				validInput = false;
+			}
+		} while(!validInput);
+
+		LocalDate resDato = LocalDate.of(1900, 01, 01);
+		do {
+			System.out.println("Indtast dato for resultatet (dd-mm-yyyy)");
+			String dato = scan.next();
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+			try {
+				validInput = true;
+				resDato = LocalDate.parse(dato, formatter);
+			} catch(DateTimeParseException e) {
+				System.out.println("Ugyldig dato!");
+				validInput = false;
+			}
+		} while(!validInput);
 
 		Traeningsresultat nytResultat = new Traeningsresultat(disciplin, tid, id, resDato);
 		Resultat.resultatListe.add(nytResultat);
+		Resultat.gemResultater();
+		System.out.print("\n\nResultatet er gemt.\n\n");
 	}
 
 	public static void registrerKonkurrenceResultat() throws InputMismatchException {
